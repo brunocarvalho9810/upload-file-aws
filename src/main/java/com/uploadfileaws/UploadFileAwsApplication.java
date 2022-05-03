8df647b8a99878506d8efebb711b77d54e7b390f
Bruno Carvalho <brunocarvalho9810@gmail.com>
2022-05-03 10:29:13 -0300

    Adicionando o esqueleto do projeto

---
+++ src/main/java/com/uploadfileaws/UploadFileAwsApplication.java
@@ -0,0 +1,13 @@
+package com.uploadfileaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFileAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFileAwsApplication.class, args);
+	}
+
+}
